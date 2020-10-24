7b4d67e548336bf41dcce6a840c297bd1308f168
chrismad26 <cmadison@chicagobooth.edu>
2020-10-24 18:40:36 +0000

    hundo on rails grade

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < ActionController::Base
 
+  def add_new
+    render({:template => "calc_templates/add.html.erb"})
+  end
+
+  def addition
+   #  Parameters: {"first_num"=>"3", "second_num"=>"4"}
+   @first = params.fetch("first_num").to_f
+   @second = params.fetch("second_num").to_f
+   @result = @first + @second
+    render({:template => "calc_templates/add_results.html.erb"})
+  end
+
+  def subtract_new
+render({:template => "calc_templates/subtract.html.erb"})
+  end
+   def subtraction
+    @first = params.fetch("first_num").to_f
+   @second = params.fetch("second_num").to_f
+   @result = @second - @first
+render({:template => "calc_templates/subtract_results.html.erb"})
+  end
+
+  def multiply_new
+ render({:template => "calc_templates/multiply.html.erb"})
+  end
+  def multiplication
+     @first = params.fetch("first_num").to_f
+   @second = params.fetch("second_num").to_f
+   @result = @second * @first
+ render({:template => "calc_templates/multiply_results.html.erb"})
+  end
+  
+  def division
+     @first = params.fetch("first_num").to_f
+   @second = params.fetch("second_num").to_f
+   @result = @first/@second
+render({:template => "calc_templates/divide_results.html.erb"})
+  end
+
+  def divide_new
+render({:template => "calc_templates/divide.html.erb"})
+  end
+
 end
