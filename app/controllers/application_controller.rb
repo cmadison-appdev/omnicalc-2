c95c2c5c02bec8c5f88cf6f11c8bef07d97a4035
chrismad26 <cmadison@chicagobooth.edu>
2020-10-24 20:38:27 +0000

    stc frame setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,42 +5,51 @@ class ApplicationController < ActionController::Base
   end
 
   def addition
-   #  Parameters: {"first_num"=>"3", "second_num"=>"4"}
-   @first = params.fetch("first_num").to_f
-   @second = params.fetch("second_num").to_f
-   @result = @first + @second
+  #  Parameters: {"first_num"=>"3", "second_num"=>"4"}
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first + @second
     render({:template => "calc_templates/add_results.html.erb"})
   end
 
   def subtract_new
-render({:template => "calc_templates/subtract.html.erb"})
+    render({:template => "calc_templates/subtract.html.erb"})
   end
-   def subtraction
+
+  def subtraction
     @first = params.fetch("first_num").to_f
-   @second = params.fetch("second_num").to_f
-   @result = @second - @first
-render({:template => "calc_templates/subtract_results.html.erb"})
+    @second = params.fetch("second_num").to_f
+    @result = @second - @first
+    render({:template => "calc_templates/subtract_results.html.erb"})
   end
 
   def multiply_new
- render({:template => "calc_templates/multiply.html.erb"})
+    render({:template => "calc_templates/multiply.html.erb"})
   end
+
   def multiplication
-     @first = params.fetch("first_num").to_f
-   @second = params.fetch("second_num").to_f
-   @result = @second * @first
- render({:template => "calc_templates/multiply_results.html.erb"})
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @second * @first
+    render({:template => "calc_templates/multiply_results.html.erb"})
   end
-  
+
   def division
-     @first = params.fetch("first_num").to_f
-   @second = params.fetch("second_num").to_f
-   @result = @first/@second
-render({:template => "calc_templates/divide_results.html.erb"})
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first/@second
+    render({:template => "calc_templates/divide_results.html.erb"})
   end
 
   def divide_new
-render({:template => "calc_templates/divide.html.erb"})
+    render({:template => "calc_templates/divide.html.erb"})
   end
+  def stc_new
+    render({:template => "street_to_coords/new.html.erb"})
+  end
+  def stc_results
+    render({:template => "street_to_coords/results.html.erb"})
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-get("/add", { :controller =>  "application", :action =>  "add_new" })
-get("/addition", { :controller =>  "application", :action =>  "addition" })
-get("/subtract", { :controller =>  "application", :action =>  "subtract_new" })
-get("/subtraction", { :controller =>  "application", :action =>  "subtraction" })
-get("/multiply", { :controller => "application", :action => "multiply_new" })
-get("/multiplication", { :controller => "application", :action => "multiplication" })
-get("/divide", { :controller => "application", :action => "divide_new" })
-get("/division", { :controller => "application", :action => "division" })  
-# get("/muggle_translate", { :controller => "application", :action => "bull" })
-# get("/street_to_coords_new", { :controller => "application", :action => "maiden" })
-# get("/street_to_weather_new", { :controller => "application", :action => "goat" })
-  
-# get("/coords_to_weather_new"", { :controller => "application", :action => "twins" })
+  get("/add", { :controller =>  "application", :action =>  "add_new" })
+  get("/addition", { :controller =>  "application", :action =>  "addition" })
+
+  get("/subtract", { :controller =>  "application", :action =>  "subtract_new" })
+  get("/subtraction", { :controller =>  "application", :action =>  "subtraction" })
+
+  get("/multiply", { :controller => "application", :action => "multiply_new" })
+  get("/multiplication", { :controller => "application", :action => "multiplication" })
+
+  get("/divide", { :controller => "application", :action => "divide_new" })
+  get("/division", { :controller => "application", :action => "division" })  
+
+  # get("/muggle_translate", { :controller => "application", :action => "bull" })
+
+  get("/street_to_coords/new", { :controller => "application", :action => "stc_new" })
+  get("/street_to_coords/results", { :controller => "application", :action => "stc_results" })
+
+  # get("/street_to_weather_new", { :controller => "application", :action => "goat" })
+    
+  # get("/coords_to_weather_new"", { :controller => "application", :action => "twins" })
+
 end
